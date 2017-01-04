d4935d20cbfcfebbfc9dea74a15573a87f56177b
darkoverlordofdata <darkoverlordofdata@gmail.com>
2017-01-03 23:59:37 -0800

    remove build_dir

--- build_dir/src/Events/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bruce/bzr/libentitas/src/Events
-# Build directory: /home/bruce/bzr/libentitas/build_dir/src/Events
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build_dir/src/Interfaces/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bruce/bzr/libentitas/src/Interfaces
-# Build directory: /home/bruce/bzr/libentitas/build_dir/src/Interfaces
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
